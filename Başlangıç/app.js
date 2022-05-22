b5c8dec6ab31f2e04f6b58ff6f29b1b08b075415
gulsahsahin <guulssahh@gmail.com>
2022-05-22 20:40:10 +0300

    first

---
+++ Başlangıç/app.js
@@ -0,0 +1,50 @@
+//Alert
+// alert("Hoşgeldiniz");
+
+
+//Confirm(onay)
+// const cevap= confirm("Emin misiniz?");
+// console.log(cevap);
+
+
+//Kullancıdan giriş alma (prompt)
+// const cevap= prompt("2 + 2 = ?");
+// if (cevap==4) console.log("Doğru Cevap");
+// else console.log("Yanlış Cevap");
+
+
+// let value;
+// value=window; //window un tüm özelliklerine bakabilirsin
+// value=window.location.host;
+// value=window.location.hostname;
+// value=window.location.port;
+// value=window.location.href;
+
+// // if(confirm("Sayfa yenilensin mi?")) window.location.reload();
+// // else console.log("Sayfa yenilenmedi");
+
+// value=window.outerHeight; //pencerenin piksel uzunluğunu hesaplar
+// value=window.outerWidth; //pencerenin piksel genişliğini hesaplar
+
+// value=window.innerHeight; //sayfa içinde çizgileri almadan uzunluğu hesaplar
+// value=window.innerWidth; //sayfa içinde console kısmını almadan genişliği hesaplar
+
+// value=window.scrollX; //scroll un X konumunu gösterir
+// value=window.scrollY; //scroll un Y konumunu gösterir
+
+// console.log(value); 
+
+
+//SCOPE Kavramı
+//Global Scope, herhangi bir fonkisyon içinde olmayan alan
+
+
+//Function Scope
+// function a(){
+//     //Function Scope
+// }
+
+// //Block Scope
+// if(){
+//     //Block Scope, if,while...
+// }
